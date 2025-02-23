32cf86f9e26495b929df52cc29ce77809fa4bf14
WillGD <56427283+willgd-git@users.noreply.github.com>
2025-02-23 23:43:08 +0700

    Add several type

--- src/types/Types.sol
+++ src/types/Types.sol
@@ -9,6 +9,11 @@ enum Status {
     EXPIRED
 }
 
+enum Side {
+    BUY,
+    SELL
+}
+
 /// @notice Enum representing the side of an lending order
 enum LendingOrderType {
     LEND,
@@ -25,11 +30,38 @@ library LendingOrderTypeLibrary {
     function opposite(LendingOrderType lendingOrderType) internal pure returns (LendingOrderType) {
         return lendingOrderType == LendingOrderType.LEND ? LendingOrderType.BORROW : LendingOrderType.LEND;
     }
+    function convertToSide(LendingOrderType lendingOrderType) internal pure returns (Side) {
+        return lendingOrderType == LendingOrderType.LEND ? Side.BUY : Side.SELL;
+    }
 }
 
 library TokenOrderTypeLibrary {
     function opposite(TokenOrderType tokenOrderType) internal pure returns (TokenOrderType) {
         return tokenOrderType == TokenOrderType.BUY ? TokenOrderType.SELL : TokenOrderType.BUY;
+    }
+    function convertToSide(TokenOrderType tokenOrderType) internal pure returns (Side) {
+        return tokenOrderType == TokenOrderType.BUY ? Side.BUY : Side.SELL;
+    }
+}
+
+library Uint256Library {
+    function toString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
     }
 }
 
